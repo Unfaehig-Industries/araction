f38a7e72941d676823c4d3a2787a6ef812621406
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2022-08-03 14:57:20 +0200

    Remove unnecessary code.

--- app/src/main/java/tech/tooz/bto/toozifier/examples/BaseToozifierFragment.kt
+++ app/src/main/java/tech/tooz/bto/toozifier/examples/BaseToozifierFragment.kt
@@ -6,13 +6,9 @@ open class BaseToozifierFragment : Fragment() {
 
     companion object {
         const val TOOZ_EVENT = "Tooz event:"
-        const val WATCH_EVENT = "Watch event:"
         const val SENSOR_EVENT = "Sensor event:"
         const val BUTTON_EVENT = "Button event:"
     }
 
     protected val toozifier = ExampleApplication.getExampleApplication().toozifier
-
-    protected fun showProgress(show: Boolean) = (activity as? MainActivity)?.showProgress(show)
-
 }
