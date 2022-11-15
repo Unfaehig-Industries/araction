49fc9296a65f08d3aef139d70da733aac91c5866
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2022-11-15 13:00:24 +0100

    Add radial menu.

---
+++ app/src/main/java/tech/unfaehig_industries/tooz/araction/radial_menu/RadialMenuLayout.kt
@@ -0,0 +1,34 @@
+package tech.unfaehig_industries.tooz.araction.radial_menu
+
+import android.content.Context
+import android.view.LayoutInflater
+import tech.unfaehig_industries.tooz.araction.BaseToozifierLayout
+import tech.unfaehig_industries.tooz.araction.SafeSensorReading
+import tech.unfaehig_industries.tooz.araction.databinding.RadialMenuLayoutBinding
+import tooz.bto.toozifier.Toozifier
+
+class RadialMenuLayout (toozifier: Toozifier) : BaseToozifierLayout(toozifier) {
+
+    // These are views that are displayed in the glasses
+    private var radialMenuView: RadialMenuLayoutBinding? = null
+
+    override fun sendFrame() {
+        sendBlankFrame()
+    }
+
+    override fun sendFrame (reading: SafeSensorReading) {
+        radialMenuView?.run {
+            layoutView = this.root
+        }
+    }
+
+    override fun sendBlankFrame() {
+        radialMenuView?.run {
+            layoutView = this.root
+        }
+    }
+
+    override fun inflateView(context: Context) {
+        radialMenuView = RadialMenuLayoutBinding.inflate(LayoutInflater.from(context))
+    }
+}
