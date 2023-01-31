0468e53700bf91935f1ae9137b46980189be3b07
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2023-01-31 13:32:03 +0100

    Add ability to highlight MainButton

--- app/src/main/java/tech/unfaehig_industries/tooz/araction/views/MainButton.kt
+++ app/src/main/java/tech/unfaehig_industries/tooz/araction/views/MainButton.kt
@@ -1,16 +1,15 @@
 package tech.unfaehig_industries.tooz.araction.views
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.view.View
 
 class MainButton : View {
 
     private var radialBoundingRect: RectF = RectF(0f,0f,100f,100f)
-    private var radius: Float = 5f
-    private var fill: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    var radius: Float = 5f
+    var fillPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val radiusHighlight: Float = 5f
 
     constructor(context: Context) : super(context) {
     }
@@ -18,14 +17,26 @@ class MainButton : View {
     constructor(context: Context, radialBoundingRect: RectF, radius: Float, fillColor: Int) : super(context) {
         this.radialBoundingRect = radialBoundingRect
         this.radius = radius
-        this.fill.apply { color= fillColor; style= Paint.Style.FILL }
+        this.fillPaint.apply { color= fillColor; style= Paint.Style.FILL }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         canvas?.run {
-            this.drawCircle(radialBoundingRect.centerX(), radialBoundingRect.centerY(), radius, fill)
+            this.drawCircle(radialBoundingRect.centerX(), radialBoundingRect.centerY(), radius, fillPaint)
         }
+    }
+
+    fun onHover(percent: Int) {
+        radius += radiusHighlight
+
+        val gradientRadius: Float = radius * percent
+        fillPaint.shader = RadialGradient(radialBoundingRect.centerX(), radialBoundingRect.centerY(), gradientRadius, Color.BLACK, Color.WHITE, Shader.TileMode.CLAMP)
+    }
+
+    fun onHoverLeave() {
+        radius -= radiusHighlight
+        fillPaint.shader = null
     }
 }
