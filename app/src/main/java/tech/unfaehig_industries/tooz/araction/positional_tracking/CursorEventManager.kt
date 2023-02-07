263bd4478536e40b5af7c8d1e1874ea14ea7b7e9
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2023-02-07 15:22:07 +0100

    Introduce a scaler for the distance and a sensitivity for scaling.

--- app/src/main/java/tech/unfaehig_industries/tooz/araction/positional_tracking/CursorEventManager.kt
+++ app/src/main/java/tech/unfaehig_industries/tooz/araction/positional_tracking/CursorEventManager.kt
@@ -1,3 +1,5 @@
+package tech.unfaehig_industries.tooz.araction.positional_tracking
+
 import android.app.Activity
 import android.content.Context
 import android.hardware.Sensor
@@ -10,36 +12,35 @@ import kotlin.math.acos
 import kotlin.math.sqrt
 
 
-class CursorEventManager(callback: SensorDataCallback, activity: Activity?) :
-    SensorEventListener {
-    private val sensorManager: SensorManager
-    private val sensor: Sensor
-    private val callback: SensorDataCallback
+class CursorEventManager : SensorEventListener {
+    private var sensorManager: SensorManager
+    private var sensor: Sensor
+    private var callback: SensorDataCallback
 
     private var zeroFacing: DoubleArray = DoubleArray(3)
     private var currentFacing: DoubleArray = DoubleArray(3)
     private var resetZeroPosition: Boolean = false
 
-    init {
+    private val distanceScaler: Float = 200f
+    private var distanceSensitivity: Float = 1f
+
+    constructor(callback: SensorDataCallback, activity: Activity?) {
         sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR)
+        sensor = sensorManager.getDefaultSensor(TYPE_GAME_ROTATION_VECTOR)
         this.callback = callback
     }
 
-    fun start() {
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
-        resetZeroPosition = true
-    }
-
-    fun stop() {
-        sensorManager.unregisterListener(this)
+    constructor(callback: SensorDataCallback, activity: Activity?, sensitivity: Float) {
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(TYPE_GAME_ROTATION_VECTOR)
+        this.callback = callback
+        this.distanceSensitivity = sensitivity
     }
 
     override fun onSensorChanged(event: SensorEvent) {
 
         require(
-            event.sensor.type == TYPE_GAME_ROTATION_VECTOR ||
-            event.sensor.type == TYPE_ROTATION_VECTOR
+            event.sensor.type == TYPE_GAME_ROTATION_VECTOR || event.sensor.type == TYPE_ROTATION_VECTOR
         ) { "Wrong sensor type received!" }
 
         val values = event.values
@@ -49,7 +50,6 @@ class CursorEventManager(callback: SensorDataCallback, activity: Activity?) :
         SensorManager.getRotationMatrixFromVector (currentRotation, values)
 
         currentFacing = rotateVectorByMatrix((currentRotation.map { it.toDouble() }).toDoubleArray(), doubleArrayOf(0.0, 1.0, 0.0))
-//        val currentUp = rotateVectorByMatrix((currentRotation.map { it.toDouble() }).toDoubleArray(), doubleArrayOf(0.0, 0.0, 1.0))
 
         if (resetZeroPosition) {
             resetZeroPosition = false
@@ -63,7 +63,7 @@ class CursorEventManager(callback: SensorDataCallback, activity: Activity?) :
         )
 
         val angle = angleBetweenVectors(directionVector, doubleArrayOf(0.0, 0.0, 1.0))
-        val dist = sqrt(directionVector.sumOf { it * it })
+        val dist = sqrt(directionVector.sumOf { it * it }) * distanceScaler * distanceSensitivity
 
         callback.onCursorUpdate(angle, dist)
     }
@@ -72,41 +72,50 @@ class CursorEventManager(callback: SensorDataCallback, activity: Activity?) :
         callback.onAccuracyChanged(accuracy)
     }
 
-    interface SensorDataCallback {
-        fun onCursorUpdate(angle: Double, dist: Double)
-        fun onAccuracyChanged(accuracy: Int)
+    fun start() {
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        resetZeroPosition = true
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
     }
 
     fun resetZeroPosition() {
         resetZeroPosition = true
     }
-}
 
-fun rotateVectorByMatrix(inputMatrix: DoubleArray, vector: DoubleArray): DoubleArray {
-    // check that the matrix and vector are conformable for matrix multiplication
+    private fun rotateVectorByMatrix(inputMatrix: DoubleArray, vector: DoubleArray): DoubleArray {
+        // check that the matrix and vector are conformable for matrix multiplication
 
-    require(inputMatrix.size == 9 && vector.size == 3) { "Matrix and vector must be 3x3 and 3D respectively" }
+        require(inputMatrix.size == 9 && vector.size == 3) { "Matrix and vector must be 3x3 and 3D respectively" }
 
-    val matrix = arrayOf(
-        doubleArrayOf(inputMatrix[0], inputMatrix[1], inputMatrix[2]),
-        doubleArrayOf(inputMatrix[3], inputMatrix[4], inputMatrix[5]),
-        doubleArrayOf(inputMatrix[6], inputMatrix[7], inputMatrix[8])
-    )
+        val matrix = arrayOf(
+            doubleArrayOf(inputMatrix[0], inputMatrix[1], inputMatrix[2]),
+            doubleArrayOf(inputMatrix[3], inputMatrix[4], inputMatrix[5]),
+            doubleArrayOf(inputMatrix[6], inputMatrix[7], inputMatrix[8])
+        )
 
-    // perform the matrix multiplication
-    val result = DoubleArray(3)
-    for (i in 0..2) {
-        for (j in 0..2) {
-            result[i] += matrix[i][j] * vector[j]
+        // perform the matrix multiplication
+        val result = DoubleArray(3)
+        for (i in 0..2) {
+            for (j in 0..2) {
+                result[i] += matrix[i][j] * vector[j]
+            }
         }
+        return result
     }
-    return result
+
+    private fun angleBetweenVectors(vector1: DoubleArray, vector2: DoubleArray): Double {
+        require(vector1.size == 3 && vector2.size == 3) { "Vectors must be 3D" }
+        val dotProduct = vector1.zip(vector2) { a, b -> a * b }.sum()
+        val magnitude1 = sqrt(vector1.sumOf { it * it })
+        val magnitude2 = sqrt(vector2.sumOf { it * it })
+        return acos(dotProduct / (magnitude1 * magnitude2))
+    }
 }
 
-fun angleBetweenVectors(vector1: DoubleArray, vector2: DoubleArray): Double {
-    require(vector1.size == 3 && vector2.size == 3) { "Vectors must be 3D" }
-    val dotProduct = vector1.zip(vector2) { a, b -> a * b }.sum()
-    val magnitude1 = sqrt(vector1.sumOf { it * it })
-    val magnitude2 = sqrt(vector2.sumOf { it * it })
-    return acos(dotProduct / (magnitude1 * magnitude2))
+interface SensorDataCallback {
+    fun onCursorUpdate(angle: Double, dist: Double)
+    fun onAccuracyChanged(accuracy: Int)
 }
