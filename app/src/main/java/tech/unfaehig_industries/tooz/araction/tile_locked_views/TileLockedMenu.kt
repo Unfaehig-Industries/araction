17ea14aec43cefe7da88452d82a39e6dc1728e32
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2023-06-28 13:39:27 +0200

    Rework tiling algorithm.

--- app/src/main/java/tech/unfaehig_industries/tooz/araction/tile_locked_views/TileLockedMenu.kt
+++ app/src/main/java/tech/unfaehig_industries/tooz/araction/tile_locked_views/TileLockedMenu.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.RectF
 import android.util.AttributeSet
 import tech.unfaehig_industries.tooz.araction.tile_views.TileButton
+import tech.unfaehig_industries.tooz.araction.tile_views.TileData
 import tech.unfaehig_industries.tooz.araction.tile_views.TileMenu
 import kotlin.math.cos
 import kotlin.math.sin
@@ -11,17 +12,22 @@ import kotlin.math.sin
 class TileLockedMenu : TileMenu {
 
     private var lastDistX: Float = 0F
-    private val menuRect: RectF = RectF(0f, 0f, screen.width(), screen.height())
+    private val menuRect: RectF = RectF(0f, 0f, 0f, 0f)
 
     constructor(context:Context) : super(context) {
-        setBoundaries()
     }
 
     constructor(context:Context, attrs:AttributeSet) : super(context, attrs) {
-        setBoundaries()
     }
 
-    private fun setBoundaries() {
+    override fun populate(
+        tiles: List<TileData>,
+        screen: RectF,
+        _sensitivityX: Float,
+        _sensitivityY: Float
+    ) {
+        super.populate(tiles, screen, _sensitivityX, _sensitivityY)
+
         for (button in tileButtons) {
 
             if (button.translationX > menuRect.right) {
@@ -39,8 +45,8 @@ class TileLockedMenu : TileMenu {
             return
         }
 
-        var distX: Float = (distance * sin(angle)).toFloat() * viewMovementFactorX
-        var distY: Float = -(distance * cos(angle)).toFloat() * viewMovementFactorY
+        var distX: Float = (distance * sin(angle)).toFloat() * sensitivityX
+        var distY: Float = -(distance * cos(angle)).toFloat() * sensitivityY
 
         // Make sure one can't go left of the first column (x=0 is at center of first column)
         if (distX < ( -buttonRect.width() / 2) ) {
--- app/src/main/java/tech/unfaehig_industries/tooz/araction/tile_views/TileMenu.kt
+++ app/src/main/java/tech/unfaehig_industries/tooz/araction/tile_views/TileMenu.kt
@@ -9,91 +9,96 @@ import tech.unfaehig_industries.tooz.araction.R
 import timber.log.Timber
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.LinkedHashMap
 import kotlin.math.cos
 import kotlin.math.sin
 
 open class TileMenu : RelativeLayout {
 
-    protected lateinit var tileButtons: ArrayList<TileButton>
+    protected var tileButtons: ArrayList<TileButton> = ArrayList()
     private var hoveredButton: TileButton? = null
     private var mainColor: Int = Color.CYAN
     private var backgroundColor: Int = Color.BLACK
-    protected val screen = RectF(0f, 0f, 390f, 528f)
-    protected val buttonRect = RectF(0f, 0f, (screen.width() / 3.5f), (screen.height() / 5))
-    protected val viewMovementFactorX: Float = 4f
-    protected val viewMovementFactorY: Float = 4f
+    protected lateinit var screenRect: RectF
+    protected lateinit var buttonRect: RectF
+    protected var sensitivityX: Float = 4f
+    protected var sensitivityY: Float = 4f
 
-    constructor(context:Context) : super(context) {
-        init()
-    }
+    constructor(context: Context) : super(context)
 
-    constructor(context:Context, attrs:AttributeSet) : super(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.ToozMenuStyleable, 0, 0)
 
         mainColor = typedArray.getColor(R.styleable.ToozMenuStyleable_mainColor, Color.CYAN)
         backgroundColor = typedArray.getColor(R.styleable.ToozMenuStyleable_backgroundColor, Color.BLACK)
-
-        init()
     }
 
-    private fun init() {
-        val menuMap = LinkedHashMap<String, Array<String>>()
-        menuMap["1"] = arrayOf("a", "b", "c", "d")
-        menuMap["2"] = arrayOf("a", "b", "c", "d")
-        menuMap["3"] = arrayOf("a", "b", "c", "d")
-        menuMap["4"] = arrayOf("a", "b", "c", "d")
-        menuMap["5"] = arrayOf("a", "b", "c", "d")
-        tileButtons = addTileButtons(menuMap)
-    }
+    open fun populate(
+        tiles: List<TileData>,
+        screen: RectF = RectF(0f, 0f, 390f, 528f),
+        _sensitivityX: Float = 4f,
+        _sensitivityY: Float = 4f
+    ) {
+        screenRect = RectF(screen)
+        buttonRect = RectF(0f, 0f, (screen.width() / 3.5f), (screen.height() / 5))
+        sensitivityX = _sensitivityX
+        sensitivityY = _sensitivityY
 
-    private fun addTileButtons(data: LinkedHashMap<String, Array<String>>): ArrayList<TileButton> {
-        val buttonsArray = ArrayList<TileButton>(data.size)
         val boundingRect = RectF(buttonRect)
-        val horizontalSpacing: Float = screen.width() / 2.5f
-        boundingRect.offsetTo(screen.centerX() - (buttonRect.width() / 2), screen.centerY() - (buttonRect.height() / 2))
+        boundingRect.offsetTo(screenRect.centerX() - (buttonRect.width() / 2), screenRect.centerY() - (buttonRect.height() / 2))
+        tileButtons = addTileButtons(tiles, Direction.HORIZONTAL, boundingRect)
+        hoveredButton = tileButtons.first()
+    }
 
-        data.forEach { (label, children) ->
-            val childrenButtons = addTileButtons(children, boundingRect)
+    private fun addTileButtons(tiles: List<TileData>, direction: Direction, boundingRect: RectF): ArrayList<TileButton> {
+        val buttonsArray = ArrayList<TileButton>(tiles.size)
 
-            val tileButton: TileButton = createTileButton(label, childrenButtons, boundingRect)
+        val horizontalSpacing: Float = screenRect.width() / 2.5f
+        val verticalSpacing: Float = (screenRect.height() / 4)
+
+        val otherDirection = when (direction) {
+            Direction.HORIZONTAL -> Direction.VERTICAL
+            Direction.VERTICAL ->  Direction.HORIZONTAL
+        }
+
+        tiles.forEachIndexed { i, tile ->
+
+            if (i > 0) {
+                when (direction) {
+
+                    Direction.HORIZONTAL -> {
+                        boundingRect.offsetTo(
+                            boundingRect.left + horizontalSpacing,
+                            boundingRect.top
+                        )
+                    }
+
+                    Direction.VERTICAL -> {
+                        boundingRect.offsetTo(boundingRect.left, boundingRect.top + verticalSpacing)
+                    }
+                }
+            }
+
+            val children = addTileButtons(tile.children, otherDirection, RectF(boundingRect))
+            val tileButton = TileButton(
+                context,
+                boundingRect,
+                RectF(buttonRect),
+                tile.title,
+                children,
+                tile.color,
+                backgroundColor
+            )
 
             buttonsArray.add(tileButton)
-            buttonsArray.addAll(childrenButtons)
-            this.addView(tileButton, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
+            buttonsArray.addAll(children)
 
-            boundingRect.offsetTo(boundingRect.left + horizontalSpacing, boundingRect.top)
+            this.addView(
+                tileButton,
+                LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+            )
         }
 
         return buttonsArray
-    }
-
-    private fun addTileButtons(data: Array<String>, originRect: RectF = RectF()): ArrayList<TileButton> {
-        val buttonsArray = ArrayList<TileButton>(data.size)
-        val boundingRect = RectF(originRect)
-        val verticalSpacing: Float = (screen.height() / 4)
-
-        if (boundingRect.isEmpty) {
-            boundingRect.offsetTo(screen.centerX() - (buttonRect.width() / 2), screen.centerY() - (buttonRect.height() / 2))
-        }
-
-        for (i in 0 until(data.size)) {
-            boundingRect.offsetTo(boundingRect.left, boundingRect.top + verticalSpacing)
-
-            val tileButton = createTileButton(data[i], ArrayList(), boundingRect)
-
-            buttonsArray.add(tileButton)
-            this.addView(tileButton, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
-        }
-
-        return buttonsArray
-    }
-
-    private fun createTileButton(label: String, children: ArrayList<TileButton>, positionRect: RectF): TileButton {
-        val rnd = Random()
-        val fillColor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
-
-        return TileButton(context, positionRect, RectF(buttonRect), label, children, fillColor, backgroundColor)
     }
 
     open fun moveView(angle: Double, distance: Double) {
@@ -101,8 +106,8 @@ open class TileMenu : RelativeLayout {
             return
         }
 
-        val distX: Float = -(distance * sin(angle)).toFloat() * viewMovementFactorX
-        val distY: Float = (distance * cos(angle)).toFloat() * viewMovementFactorY
+        val distX: Float = -(distance * sin(angle)).toFloat() * sensitivityX
+        val distY: Float = (distance * cos(angle)).toFloat() * sensitivityY
 
         tileButtons.forEach { button: TileButton ->
             button.animate().translationX(button.baseX + distX)
@@ -113,7 +118,7 @@ open class TileMenu : RelativeLayout {
     }
 
     protected fun highlightButton() {
-        if (hoveredButton?.isInCenter(screen) == true) {
+        if (hoveredButton?.isInCenter(screenRect) == true) {
             return
         }
 
@@ -121,7 +126,7 @@ open class TileMenu : RelativeLayout {
         hoveredButton = null
 
         for (button in tileButtons) {
-            if (button.isInCenter(screen)) {
+            if (button.isInCenter(screenRect)) {
                 if (hoveredButton != button) {
                     button.isHovered = true
 
@@ -133,4 +138,8 @@ open class TileMenu : RelativeLayout {
             }
         }
     }
+}
+
+enum class Direction {
+    HORIZONTAL, VERTICAL
 }
